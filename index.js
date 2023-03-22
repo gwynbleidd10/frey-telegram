a3cbd7c12a4cd7a28e23b33223067ff925aaab45
Vadim Rudykh <gwynbleidd10@gmail.com>
2023-03-22 11:50:43 +0900

    test

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-const { Tg_chats, Tg_tokens } = require('@/models/pg/10.18.100.65/system')
-
-// const { sendErrorMessage } = require('@/utils')
-const { responseMessage, sendFile } = require('./methods')
-
-const path = 'utils/telegram'
-
 /**
  * 
- * @param {Object} token
- * @param {Object} type
- * @param {Object} sendMessage 
+ * @param {Object} {token = null, chat = null, message = null} 
  * @returns 
  */
 const Telegram = async ({
-    bot,
-    chat = null,
+    token: globalToken = null,
+    chat: globalChat = null,
+    message: globalMessage = null,
 }) => {
-    let globalToken = await getToken(bot) || null,
-        globalChat = await getChat(chat) || null,
-        response = { status: true, message: null },
-        header = {
-            parse_mode: 'html',
-            disable_web_page_preview: true,
+    const isCommand = async (options = {
+        message: globalMessage.text
+    }) => {
+        const newMessage = options.message
+
+        if (newMessage) {
+            const reg = /^\/.+$/gmi
+
+            if (reg.test(newMessage)) {
+                let messageArr = newMessage.split(' ')
+                return { command: messageArr[0], text: messageArr.slice(1).join(' ') }
+            }
+            else {
+                return false
+            }
         }
+        else {
+            return 'Отсутствует поле MESSAGE'
+        }
+    }
 
     const sendMessage = async ({
         token: newToken = globalToken,
@@ -33,169 +38,55 @@ const Telegram = async ({
         inline_keyboard = [],
         keyboard = [],
     }) => {
-        const url = `https://api.telegram.org/bot${newToken}/sendMessage`
+        if (newToken && newChat) {
+            const url = `https://api.telegram.org/bot${newToken}/sendMessage`
 
-        let result = { status: false, message: null }
+            let result = { status: false, message: null }
 
-        let send = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                ...header,
-                chat_id: newChat,
-                text,
-                reply_markup: {
-                    inline_keyboard: [
-                        [...inline_keyboard]
-                    ],
-                    keyboard: [
-                        ...keyboard
-                    ],
-                    resize_keyboard,
-                    one_time_keyboard,
-                }
+            let send = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    parse_mode: 'html',
+                    disable_web_page_preview: true,
+                    chat_id: newChat,
+                    text,
+                    reply_markup: {
+                        inline_keyboard: [
+                            [...inline_keyboard]
+                        ],
+                        keyboard: [
+                            ...keyboard
+                        ],
+                        resize_keyboard,
+                        one_time_keyboard,
+                    }
+                })
             })
-        })
 
-        send = await send.json()
+            send = await send.json()
 
-        if (send.ok)
-            result = result?.ok
+            if (send.ok)
+                result = result?.ok
 
-        return result
-    }
-
-    if (globalToken) {
-        return {
-            sendMessage
+            return result
         }
-    }
-    else {
-
-    }
-
-    // return response
-
-    // let requestType = await getType(request) || null,
-    //     isCommand = false,
-    //     command = null
-
-    // if (requestType == 'message') {
-    //     let check = await checkCommand(request.message.text)
-    //     isCommand = check.isCommand
-    //     command = check.command
-    // }
-
-    // if (requestType == 'document') {
-    //     let check = await checkCommand(request.message.caption)
-    //     isCommand = check.isCommand
-    //     command = check.command
-    // }
-
-    // return {
-    //     token: botToken,
-    //     chat: botChat,
-    //     type: requestType,
-    //     isCommand,
-    //     command,
-    //     request,
-    //     sendMessage,
-    //     responseMessage,
-    //     sendFile,
-    // }
-}
-
-const getToken = async (bot) => {
-    let result = null
-
-    try {
-        let getToken = await Tg_tokens.findOne({
-            raw: true,
-            where: {
-                name: bot
-            }
-        })
-
-        if (getToken) {
-            result = getToken.token
-        }
-    } catch (err) {
-        // await sendErrorMessage({ path, message: 'Ошибка получения токена', stack: err.stack, send: false, log: true })
-    }
-
-    return result
-}
-
-const getChat = async (chat) => {
-    let result = null
-
-    try {
-        let getChat = await Tg_chats.findOne({
-            raw: true,
-            where: {
-                name: chat
-            }
-        })
-
-        if (getChat) {
-            result = getChat.chat
-        }
-    } catch (err) {
-        // await sendErrorMessage({ path, message: 'Ошибка получения чата', stack: err.stack, send: false, log: true })
-    }
-
-    return result
-}
-
-const getType = async (request) => {
-    let type = null
-
-    if (request?.message) {
-        let message = request?.message
-        if (message?.document) {
-            type = 'document'
+        else {
+            return { status: false, message: 'Необходимо указать TOKEN и CHAT бота' }
         }
 
-        if (message?.text) {
-            type = 'message'
-        }
-    }
-
-    if (request?.callback_query) {
-        type = 'callback'
-    }
-
-    return type
-}
-
-const checkCommand = async (text) => {
-    const reg = /^\/[a-zA-Zа-яА-Я0-9_]+(\ .+)*$/gmi,
-        regInline = /^\/[a-zA-Zа-яА-Я0-9_]+(\@.+)+$/gmi
-
-    let isCommand = false,
-        command = null
-
-    if (reg.test(text)) {
-        isCommand = true
-        command = text.split(' ')[0].split('/')[1]
-    }
-
-    if (regInline.test(text)) {
-        isCommand = true
-        command = text.split('@')[0].split('/')[1]
     }
 
     return {
+        token: globalToken,
+        chat: globalChat,
+        message: globalMessage,
         isCommand,
-        command
+        sendMessage,
     }
-}
-
-const webhookResponse = async () => {
-
 }
 
 module.exports = {
